cda37c82912a482dc1eef35e8215b3a38b49ab41
Tien <viettienha98@gmail.com>
2020-06-12 17:27:50 +0700

    On progress: Image upload

--- frontend/src/component/Post/PostUpload.js
+++ frontend/src/component/Post/PostUpload.js
@@ -1,10 +1,45 @@
 import React, { useState } from "react";
 import { Card, Input, Button } from "antd";
+import { Upload, Modal } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import FirebaseController from "../../firebase.js";
 
 function PostUpload() {
   const { TextArea } = Input;
   const [status, setStatus] = useState("");
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
+
+  function getBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  }
+
+  handleCancel = () => setPreviewVisible(false);
+
+  handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+
+    setPreviewImage(file.url || file.preview);
+    setPreviewVisible(true);
+    setPreviewTitle(
+      file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
+    );
+  };
+
+  const uploadImageButton = (
+    <div>
+      <PlusOutlined />
+      <div className="ant-upload-text">Upload</div>
+    </div>
+  );
 
   const uploadPost = () => {
     let data = {
